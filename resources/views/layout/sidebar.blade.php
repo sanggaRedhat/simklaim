1227c6e9bcf837db5e0b24bcebb0dc34991d8255
Sangga Daya Anugerah <sangga.redhat@gmail.com>
2021-06-03 17:06:33 +0700

    first

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,3 @@
+<ul class="nav nav-pills nav-sidebar nav-flat flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+</ul>
---
+++ resources/views/page/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.index')
+@section('header')
+
+
+    <h1 class="m-0"></h1>
+@endsection
+@section('page')
+    <div class="row">
+
+    </div>
+@endsection
